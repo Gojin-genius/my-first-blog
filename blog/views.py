db37e69854e49203df58b75f7fee97a4faf50f96
admin <admin@admin.com>
2026-05-20 13:10:03 +0900

    시 페이지 추가

--- blog/views.py
+++ blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def post_list(request):
     return render(request, 'blog/post_list.html', {})
+
+def poem_page(request):
+    return render(request, 'blog/poem.html', {})
