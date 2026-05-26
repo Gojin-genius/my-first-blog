f1d0c0a1e6aafb8692d71a2dac0bd11736ecf34e
admin <admin@admin.com>
2026-05-27 00:52:50 +0900

    Added views to create/edit blog post inside the site.

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('', views.post_list, name = 'post_list'),
     path('poem/', views.poem_page, name='poem_page'),
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/new/', views.post_new, name='post_new'),
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
